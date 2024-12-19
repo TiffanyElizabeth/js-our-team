7831d67a6ae577f3b4ca063cfb0c4e8da09ac64d
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2024-12-19 22:12:24 +0100

    feat: complete base project (not bonus)

--- script.js
+++ script.js
@@ -3,36 +3,61 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "female3.png"
   }
 ];
+
+
+// DOM ELEMENTS
+const teamListElm = document.getElementById("team-list");
+const teamContainer = document.querySelector(".team-container");
+
+// MEMBER LIST
+let listItems = "";
+for(let i = 0; i < teamMembers.length; i++) {
+  const member = teamMembers[i];
+  listItems += `
+    <div class="team-card">
+      <div class="card-image">
+        <img src="img/${member.img}" alt=${member.name}>
+      </div>
+      <div class="card-text">
+        <h3>${member.name}</h3>
+        <p>${member.role}</p>
+        <a href=${member.email}>${member.email}</a>
+      </div>
+    </div>`;
+}
+
+teamContainer.innerHTML = listItems;
+
